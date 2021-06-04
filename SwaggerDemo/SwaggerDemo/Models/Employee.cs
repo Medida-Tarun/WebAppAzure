16dce207529aab28bcdd806bb3304760b93d0c55
Tarun <tarun.dimpu@gmail.com>
2021-06-04 16:03:16 +0530

    Initial commit

---
+++ SwaggerDemo/SwaggerDemo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerDemo.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Url { get; set; }
+    }
+}
